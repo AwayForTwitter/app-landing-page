b4c7e1b26d4cd7528f7a5f2d3d7a144e3977e354
Marina Gornostaeva <marina@hybridcattt.com>
2023-02-02 20:08:34 +0100

    Add test log

--- netlify/edge-functions/app_preflight_dev.ts
+++ netlify/edge-functions/app_preflight_dev.ts
@@ -1,5 +1,6 @@
 import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
+  console.log("Test Log")
   return Response.json({ "deprecated_production": true, "deprecation_text": "Twitter has made the decision to shut down the API 😔 While expected, it is nonetheless sad. Please keep the app installed - I'll be adding support for Mastodon, and you will keep the PRO unlock, if you've purchased it.", "deprecation_tap_url": "https://google.com" });
 };
