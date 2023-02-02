d0c86e9ab7a6cc05e81b8ad607a234ea6a74243e
Marina Gornostaeva <marina@hybridcattt.com>
2023-02-02 19:45:28 +0100

    Test deprecation notice

--- netlify/edge-functions/app_preflight_dev.ts
+++ netlify/edge-functions/app_preflight_dev.ts
@@ -1,5 +1,5 @@
 import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
-  return Response.json({ hello: "world" });
+  return Response.json({ "deprecated_production": true, "deprecation_text": "Twitter has made the decision to shut down the API 😔 While expected, it is nonetheless sad. Please keep the app installed - I'll be adding support for Mastodon, and you will keep the PRO unlock, if you've purchased it.", "deprecation_tap_url": "https://google.com" });
 };
