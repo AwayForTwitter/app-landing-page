4b5601b256d3dd188300337cbe4ddef403120529
Marina Gornostaeva <marina@hybridcattt.com>
2023-02-02 22:33:55 +0100

    Try

--- netlify/edge-functions/app_preflight_dev.ts
+++ netlify/edge-functions/app_preflight_dev.ts
@@ -4,7 +4,9 @@ export default async (request: Request, context: Context) => {
   const token = Deno.env.get("GOAT_TOKEN");
   
   const url = new URL(request.url)
-  const search = url.search
+  const search = url.search || "none"
+  const decoded = decodeURIComponent(search)
+
   const params = new URLSearchParams(search)
   const version = params.get('v') || "none"
 
@@ -12,12 +14,12 @@ export default async (request: Request, context: Context) => {
     method: 'POST',
     body: JSON.stringify({
       no_sessions: true,
-      "hits": [{"event": true, "path": "/app_preflight_dev", "ref": `v=${version}`}],
+      "hits": [{"event": true, "path": "/app_preflight_dev", "ref": `q=${decoded}`}],
     }),
     headers: {
       'Content-Type': 'application/json; charset=UTF-8', 
       'Authorization': `Bearer ${token}`
     } 
   });
-  return Response.json({ "deprecated_production": true, "deprecation_text": "Twitter has made the decision to shut down the API 😔 While expected, it is nonetheless sad. Please keep the app installed - I'll be adding support for Mastodon, and you will keep the PRO unlock, if you've purchased it.", "deprecation_tap_url": "https://google.com" });
+  return Response.json({ "deprecated_production": true, "deprecation_text": `${decoded} Twitter has made the decision to shut down the API 😔 While expected, it is nonetheless sad. Please keep the app installed - I'll be adding support for Mastodon, and you will keep the PRO unlock, if you've purchased it.`, "deprecation_tap_url": "https://google.com" });
 };
